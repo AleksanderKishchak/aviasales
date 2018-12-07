013cc8d4288f8b7a8672d53da3f3622bfb75227f
Aleksander Kishchak <sanya69876@gmail.com>
2018-12-07 15:11:58 +0200

    setup project

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import tickets from './data/tickets';
 import './App.css';
+/*eslint-disable */
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      tickets
+    }
+  }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <div>ghd</div>
   }
 }
 
