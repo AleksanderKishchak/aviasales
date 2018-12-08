1fc75986e0e98a8d61e1d4cfcdf451e5cb580cd4
Aleksander Kishchak <sanya69876@gmail.com>
2018-12-08 22:05:26 +0200

    Updates

---
+++ precache-manifest.608f737a6e17b8c526b170bfa287bdd2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "52bc738cd91f1411ce11f4cde7948327",
+    "url": "/aviasales/static/media/turkish-airlines_logo.52bc738c.png"
+  },
+  {
+    "revision": "8490a9b4ee19fadab235e947d0e935e5",
+    "url": "/aviasales/static/media/check-mark.8490a9b4.svg"
+  },
+  {
+    "revision": "3e60741631b14f3f3284601b70003db0",
+    "url": "/aviasales/static/media/airplane_icon.3e607416.svg"
+  },
+  {
+    "revision": "06ec94990c2f538dc642",
+    "url": "/aviasales/static/js/runtime~main.06ec9499.js"
+  },
+  {
+    "revision": "d957a2a01b0912b9801e",
+    "url": "/aviasales/static/js/main.d957a2a0.chunk.js"
+  },
+  {
+    "revision": "01108cc1f18ca2fa6de1",
+    "url": "/aviasales/static/js/1.01108cc1.chunk.js"
+  },
+  {
+    "revision": "d957a2a01b0912b9801e",
+    "url": "/aviasales/static/css/main.fb305864.chunk.css"
+  },
+  {
+    "revision": "95e9ef45856224189c476a575491c1bb",
+    "url": "/aviasales/index.html"
+  }
+];
