4ac7abe6839ff832624e3cab59bdc91e1a3201a5
Aleksander Kishchak <sanya69876@gmail.com>
2018-12-19 21:35:36 +0200

    added sorting by ticket' price

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
 
     this.state = {
       currentCurrency: 'RUB',
-      filteredTickets: ticketsList,
+      filteredTickets: [],
       showAll: true,
       stops: {
         0: true,
@@ -24,6 +24,18 @@ class App extends Component {
         3: true
       }
     }
+  }
+
+  componentDidMount() {
+    this.fetchTickets();
+  }
+
+  fetchTickets = () => {
+    const sortedTicketsList = ticketsList.sort((a, b) => a.price - b.price);
+    console.log(sortedTicketsList);
+    this.setState({
+      filteredTickets: sortedTicketsList
+    });
   }
 
   handleCurrencyChange = e => {
