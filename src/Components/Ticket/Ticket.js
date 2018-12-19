193fd8b5e8bf6bda016f48876ec2203879d5dbed
Aleksander Kishchak <sanya69876@gmail.com>
2018-12-19 21:23:40 +0200

    fixed bugs with invalid date

--- src/Components/Ticket/Ticket.js
+++ src/Components/Ticket/Ticket.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Moment from 'react-moment';
+import moment from 'moment';
 import 'moment-timezone';
 import 'moment/locale/ru';
 import logo from '../../img/turkish-airlines_logo.png';
@@ -8,13 +8,17 @@ import getConvertedPrice from '../../common/convertCurrency';
 import './Ticket.sass';
 
 function getStopsString(stops) {
+  let stopsString;
+
   switch (stops) {
     case 0: break;
-    case 1: return '1 пересадка';
-    case 2: return '2 пересадки';
-    case 3: return '3 пересадки';
-    default: return 'больше трех пересадок';
+    case 1: stopsString = '1 пересадка'; break;
+    case 2: stopsString = '2 пересадки'; break;
+    case 3: stopsString = '3 пересадки'; break;
+    default: stopsString = 'больше трех пересадок';
   }
+
+  return stopsString;
 }
 
 function Ticket({
@@ -34,6 +38,8 @@ function Ticket({
 }) {
   const stopsString = getStopsString(stops);
   const convertedPrice = getConvertedPrice(price, currentCurrency);
+  const departureDate = moment(departure_date, 'MM.DD.YY');
+  const arrivalDate = moment(arrival_date, 'MM.DD.YY');
 
   return (
     <div className="ticket">
@@ -55,11 +61,7 @@ function Ticket({
           <div className="ticket-time">{ departure_time }</div>
           <div className="ticket-place">{origin}, {origin_name}</div>
           <div className="ticket-date">
-            <Moment
-              format="D MMM YYYY, ddd"
-              date={departure_date}
-              interval={0}
-            />
+            {departureDate.format('D MMM YYYY, ddd')}
           </div>
         </div>
         <div className="ticket-stops">
@@ -70,11 +72,7 @@ function Ticket({
           <div className="ticket-time">{arrival_time}</div>
           <div className="ticket-place">{destination_name}, {destination}</div>
           <div className="ticket-date">
-            <Moment
-              format="D MMM YYYY, ddd"
-              date={arrival_date}
-              interval={0}
-            />
+            {arrivalDate.format('D MMM YYYY, ddd')}
           </div>
         </div>
       </div>
